33729de053be28920b75d77bc31c577cb12c9ee9
unknown <englishtea21@mail.ru>
2023-08-01 17:40:25 +0600

    reset db

--- base/signals.py
+++ base/signals.py
@@ -10,20 +10,3 @@ def handle_room_deletion(sender, instance, **kwargs):
 
     for topic in topics_without_rooms:
         topic.delete()
-
-
-# @receiver(post_delete, sender=User)
-# def handle_profile_picture_deletion(sender, instance, **kwargs):
-#     instance.profile_picture = models.ImageField(null=False, blank=True, default='avatar.svg')
-
-# @receiver(post_save, sender=User)
-# def handle_field_change(sender, instance, **kwargs):
-#     # if not created:  # Выполнять действия только при обновлении объекта
-#     #     previous_field_value = User.objects.get(pk=instance.pk).profile_picture
-#     # print('** Hello! I am image signal **')
-#     # checking if image is empty
-#     if not bool(instance.profile_picture):
-#         instance.profile_picture = models.ImageField(null=False, blank=True, default='avatar.svg')
-#         # Действия, которые нужно выполнить при изменении поля field_name
-#         # Замените field_name на имя конкретного поля в вашей модели
-#         print("* Default avatar *")
